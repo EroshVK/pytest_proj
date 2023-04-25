9f86f595103d7d75a3973fe0325c31712d81663f
EroshVK <635753051vika@gmai.com>
2023-04-25 20:17:37 +0300

    coverage 100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -9,6 +9,8 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 1) == []
     assert arrs.my_slice([1, 2, 3, 4, 5], -3) == [3, 4, 5]
     assert arrs.my_slice([1, 2, 3, 4, 5], -4, 4) == [2, 3, 4]
     assert arrs.my_slice([1, 2, 3, 4, 5], -5, -2) == [1, 2, 3]
+    assert arrs.my_slice([1, 2, 3], -4,) == [1, 2, 3]
